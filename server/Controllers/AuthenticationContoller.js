b745bd3b9cbda004b34a2bda827ce333c7c37a90
Ruth Newman <ruthnewman@gmail.com>
2020-07-10 17:35:32 +0100

    looking for existing users with the same email address|

--- server/Controllers/AuthenticationContoller.js
+++ server/Controllers/AuthenticationContoller.js
@@ -1,3 +1,21 @@
+const User = require('../Models/User');
+
 exports.signup = function(request, response, next) {
-    response.send({ success: true });
+    // See if a user with the given email address exists already
+    const email = request.body.email;
+    const password = request.body.password;
+
+    User.findOne({ email: email }, function(error, existingUser) {
+
+    });
+
+     // If a user with the same email address exists, return a relevant error
+    
+    // If a user with the same email does NOT exist, create a user record
+
+
+
+    // Respond to the request indicating a user was created
+
+
 }
