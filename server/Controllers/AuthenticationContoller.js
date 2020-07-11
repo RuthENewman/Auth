237ce13f1fdeedd30538a8b2f4c2802b6142fb56
Ruth Newman <ruthnewman@gmail.com>
2020-07-11 19:10:05 +0100

    salting passwords

--- server/Controllers/AuthenticationContoller.js
+++ server/Controllers/AuthenticationContoller.js
@@ -5,17 +5,32 @@ exports.signup = function(request, response, next) {
     const email = request.body.email;
     const password = request.body.password;
 
+    if (!email || !password) {
+        return response.status(422).send({ error: "Both email and password are required" });
+    }
+
     User.findOne({ email: email }, function(error, existingUser) {
+        if (error) {
+            return next(error);
+        }
+        if (existingUser) {
+            return response.status(422).send({ error: "Email address already in use."});
+        }
+
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(error) {
+            if (error) {
+                return next(error);
+            }
+        // Respond to the request indicating a user was created
+            response.json({ success: true });
+        });
+
 
     });
-
-     // If a user with the same email address exists, return a relevant error
-    
-    // If a user with the same email does NOT exist, create a user record
-
-
-
-    // Respond to the request indicating a user was created
-
 
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
+});
+
+// On save hook, encrypt password
+userSchema.pre('save', function(next) {
+    // Get access to the user model
+    const user = this;
+
+    // Generate a salt
+    bcrypt.genSalt(10, function(error, salt) {
+        if (error) {
+            return next(error);
+        }
+
+        // Hash password using the salt
+        bcrypt.hash(user.password, salt, null, function(error, hash) {
+            if (error) {
+                return next(error);
+            }
+
+            user.password = hash;
+            next();
+        })
+    });
 });
 
 // Create the model class
