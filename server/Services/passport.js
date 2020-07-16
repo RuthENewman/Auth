ddfb428a6e050709cc4e58a401700514f8e334c9
Ruth Newman <ruthnewman@gmail.com>
2020-07-16 02:17:23 +0100

    set up and use jwtStrategy with passport

--- server/Services/passport.js
+++ server/Services/passport.js
@@ -3,3 +3,29 @@ const User = require('../Models/User');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Set up options for JWT Strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    // See if the user ID in the payload exists in the database
+    User.findById(payload.sub, funtion(error, user){
+        if (error) {
+            return done(error, false)
+        }
+    });
+    // If it doesn't call done without a user object
+    if (user) {
+        done(null, user);
+    // If it does, call done with the user
+    } else {
+        done(null, false);
+    }
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
