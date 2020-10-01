45cd8abb909a000fbcdf410461bcfafe00ae7b67
Ruth Newman <ruthnewman@gmail.com>
2020-10-01 17:56:34 +0100

    added Feature component and boilerplate HOC

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,14 @@
-import { AUTH_USER } from './types';
+import axios from 'axios';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = ({ email, password }) => dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
+    try {
+        const response = await axios.post('http://localhost:3090', formProps);
 
+        dispatch({ type: AUTH_USER, payload: response.data.token });
+        callback();
+    } catch (error) {
+        dispatch({ type: AUTH_ERROR, payload:  'Email in use'});
+    }
+   
 }
