127c8ef5c9323cec914b52a2af9374ab30836a61
Ruth Newman <ruthnewman@gmail.com>
2020-10-05 23:28:55 +0100

    conditional rendering of header links depending on authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './HeaderStyle.css';
 
 class Header extends Component {
+    renderLinks() {
+        if (this.props.authenticated) {
+            return (
+                <div>
+                    <Link to="/logout">Log out</Link>
+                    <Link to="/feature">Feature</Link>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Link to="/signup">Sign up</Link>
+                    <Link to="/login">Log in</Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <Link to="/">Home</Link>
-                <Link to="/signup">Sign up</Link>
-                <Link to="/login">Log in</Link>
-                <Link to="/logout">Log out</Link>
-                <Link to="/feature">Feature</Link>
+                {this.renderLinks()}
             </div>
         );
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
